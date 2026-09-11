24a9905440a13c2714fdec6fea9cdb28ef466556
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-11 16:51:40 +0100

    Test command action form updates shared Situation Room records

---
+++ test/command_actions_widget_test.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:polisphere/benue/benue_app.dart';
+
+void main() {
+  testWidgets('creating an LGA incident immediately appears in Situation Room',
+      (tester) async {
+    tester.view.physicalSize = const Size(1700, 1100);
+    tester.view.devicePixelRatio = 1;
+    addTearDown(tester.view.resetPhysicalSize);
+    addTearDown(tester.view.resetDevicePixelRatio);
+
+    await tester.pumpWidget(const BenueCampaignApp());
+    await tester.pumpAndSettle();
+
+    await tester.tap(find.text('Benue Map').first);
+    await tester.pumpAndSettle();
+    await tester.tap(find.text('Demonstrate with Makurdi'));
+    await tester.pumpAndSettle();
+
+    expect(find.textContaining('EDITING MAKURDI'), findsOneWidget);
+
+    await tester.tap(find.text('New incident'));
+    await tester.pumpAndSettle();
+
+    final titleField = find.byWidgetPredicate(
+      (widget) =>
+          widget is TextField &&
+          widget.decoration?.labelText == 'Incident title *',
+    );
+    expect(titleField, findsOneWidget);
+    await tester.enterText(titleField, 'New Makurdi test incident');
+
+    await tester.tap(find.text('Create incident'));
+    await tester.pumpAndSettle();
+
+    await tester.tap(find.text('Situation Room').first);
+    await tester.pumpAndSettle();
+
+    expect(find.text('New Makurdi test incident'), findsOneWidget);
+    expect(find.textContaining('INC-BEN-LGA-13-'), findsWidgets);
+  });
+}
