ba30d737fbb3499b80b40e6d13dc82df64d2e186
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-11 17:03:50 +0100

    Update command action test for role login

--- test/command_actions_widget_test.dart
+++ test/command_actions_widget_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:polisphere/benue/benue_app.dart';
+
+import 'ui_test_helpers.dart';
 
 void main() {
   testWidgets('creating an LGA incident immediately appears in Situation Room',
@@ -10,8 +11,7 @@ void main() {
     addTearDown(tester.view.resetPhysicalSize);
     addTearDown(tester.view.resetDevicePixelRatio);
 
-    await tester.pumpWidget(const BenueCampaignApp());
-    await tester.pumpAndSettle();
+    await pumpLoggedInAsDirectorGeneral(tester);
 
     await tester.tap(find.text('Benue Map').first);
     await tester.pumpAndSettle();
